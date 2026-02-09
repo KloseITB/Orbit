2dd1f02f493b39efc0a97b67368fc3c4663adb6e
Mauro Artale <mauro.artale01@universitadipavia.it>
2026-02-09 12:47:40 +0100

    UI development started
    started the implementation of the UI using a Facade pattern as a middleware.

---
+++ src/it/unipv/posfw/orbit/view/FacadeUserInterface.java
@@ -0,0 +1,9 @@
+package it.unipv.posfw.orbit.view;
+
+public class FacadeUserInterface {
+
+	public FacadeUserInterface() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/it/unipv/posfw/orbit/view/UI/Login.java
@@ -0,0 +1,11 @@
+package it.unipv.posfw.orbit.view.UI;
+
+import javax.swing.*;
+
+public class Login {
+
+	public Login() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
